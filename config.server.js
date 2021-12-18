c24400096934760a889db2b42b63927370a6230e
Martin Suter <mac.xander86@gmail.com>
2021-12-18 11:17:37 +0100

    fix wrong versioning

--- config.server.js
+++ config.server.js
@@ -6,6 +6,6 @@ if(process.env.WEB_PORT) config.WEB_PORT = process.env.WEB_PORT;
 if(process.env.SERVER_IP) config.SERVER_IP = process.env.SERVER_IP;
 if(process.env.SERVER_KEY) config.SERVER_KEY = process.env.SERVER_KEY;
 
-config.VERSION = '2.0';
+config.VERSION = process.env.npm_package_version;
 
 module.exports = config;

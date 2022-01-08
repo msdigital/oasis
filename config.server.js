012c20230148eca2e7e9066019be98551d2efdb9
Martin Suter <mac.xander86@gmail.com>
2022-01-08 23:20:04 +0100

    added check for config

--- config.server.js
+++ config.server.js
@@ -1,10 +1,24 @@
-require('dotenv').config();
+const { exitOnError } = require('./server/lib/logger');
+var logger = require('./server/lib/logger');
+
+require('dotenv').config()
 
 var config = {};
+var errored = false;
 
-if(process.env.WEB_PORT) config.WEB_PORT = process.env.WEB_PORT;
-if(process.env.SERVER_IP) config.SERVER_IP = process.env.SERVER_IP;
-if(process.env.SERVER_KEY) config.SERVER_KEY = process.env.SERVER_KEY;
+config.WEB_PORT = process.env.WEB_PORT;
+config.SERVER_IP = process.env.SERVER_IP;
+config.SERVER_KEY = process.env.SERVER_KEY;
+
+// put required parameters befor this check to ensure they are checked
+for (const [k,v] of Object.entries(config)){
+  if (v == "" || typeof v == 'undefined'){
+    logger.error(k+" missing! Please check configuration!");
+    errored = true;
+  }
+}
+
+if (errored) process.exit();
 
 config.VERSION = process.env.npm_package_version;
 
